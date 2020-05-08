c8fed58ae24e13e8b8b62fb91c8cd506d5908fc5
Aleksandr <45813731+AlexFigures@users.noreply.github.com>
2020-05-08 11:35:08 +0300

    add 404 redirect

--- app/core/router.php
+++ app/core/router.php
@@ -77,8 +77,6 @@ class Router {
 
    static function page404(){
 	    $host = 'https://'.$_SERVER['HTTP_HOST'].'/';
-        header('HTTP/1.1 404 Not Found');
-        header("Status: 404 Not Found");
         header('Location:'.$host.'404');
    }
 	

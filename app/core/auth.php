eeda59522616ad06189c0ae31b84381dc2cf02c9
Aleksandr <45813731+AlexFigures@users.noreply.github.com>
2020-05-11 17:36:27 +0300

    add registration

--- app/core/auth.php
+++ app/core/auth.php
@@ -12,6 +12,14 @@ Class Auth {
         else return false;
     }
 
+    static function checkRights(){
+        if (isset($_SESSION["rights"])){
+            return $_SESSION["rights"];
+        } else {
+            return false;
+        }
+    }
+
     public function authUser($username,$pass){
         $this->username = $username;
         $this->pass = $pass;
@@ -32,23 +40,69 @@ Class Auth {
 
             $stmt = $dbConnection->prepare("UPDATE users SET time=now(), uuid=:token WHERE username=:username AND pass=:pass");
             $stmt->execute(array(
-                    ':username' => $this->username,
-                    ':pass' => md5(md5($this->pass)),
-                    ':token' => $token
-             ));
+                ':username' => $this->username,
+                ':pass' => md5(md5($this->pass)),
+                ':token' => $token
+            ));
 
         } else {
-        $_SESSION["is_auth"] = false;
+            $_SESSION["is_auth"] = false;
         }
         header('Content-type: application/json');
         echo json_encode(['is_auth' => $_SESSION['is_auth']]);
-}
+    }
+
+    public function register($username,$pass,$email){
+        global $dbConnection;
+        try{
+            $stmt = $dbConnection->prepare('INSERT INTO users (username, pass, uuid, email) VALUES (:username, :pass, :uuid, :email)');
+            $stmt->execute(array(
+                ':username' => $username,
+                ':pass' => md5(md5($pass)),
+                ':uuid' => bin2hex(random_bytes(16)),
+                ':email' => $email
+            ));
+        }catch(PDOException $e){
+            echo 'Error : '.$e->getMessage();
+            exit();
+        }
+        return true;
+    }
+
+    public function checkEmail($email){
+        global $dbConnection;
+        $stmt = $dbConnection->prepare('SELECT email from users where email=:email');
+        $stmt->execute(array(
+            ':email' => $email
+        ));
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($stmt->rowCount() == 1){
+            return false;
+        } else {
+            return true;
+        }
+        header('Content-type: application/json');
+    }
+
+    public function checkLogin($username){
+        global $dbConnection;
+        $stmt = $dbConnection->prepare('SELECT username from users where username=:username');
+        $stmt->execute(array(
+            ':username' => $username
+        ));
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($stmt->rowCount() == 1){
+            return false;
+        } else {
+            return true;
+        }
+
+    }
 
     public function logOut() {
         setcookie('login', '', time()-3600, '/');
         $_SESSION = [];
         session_destroy();
-        echo 'logout!';
     }
 
 }
